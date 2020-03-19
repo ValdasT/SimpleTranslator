d9b96c01ca286b4d0acf70be79f9217c026ec73f
Valdas-Trakumas <valdas.trakumas@ibm.com>
2020-03-19 14:05:24 +0200

    init commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace SimpleTranslator {
+    class Program {
+        static async Task Main (string[] args) {
+
+            var url = "https://translate.googleapis.com/translate_a/single?client=gtx&sl=en&tl=ru&dt=t&q=hello";
+            using var client = new HttpClient ();
+
+            var response = await client.GetAsync (url);
+
+            string result = response.Content.ReadAsStringAsync ().Result;
+            var output = JsonConvert.DeserializeObject<List<dynamic>> (result);
+            Console.WriteLine (output[0][0][0]);
+        }
+    }
+}
